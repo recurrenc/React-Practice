098d8449cb5dfa1b110d237900c257bc1150e2ad
Sonu Kumar <sonu.patna0808@gmail.com>
2021-05-09 08:09:55 +0530

    Dynamic Card Generator

--- src/components/TopNetflixSeriese/ListData.js
+++ src/components/TopNetflixSeriese/ListData.js
@@ -31,6 +31,134 @@ const ListData = [
     imgsrc:
       "https://cdn.pastemagazine.com/www/articles/2020/09/11/im-thinking-of-ending.jpg",
   },
+  {
+    title: "Lady Bird",
+    director: "Greta Gerwig",
+    stars:
+      "Saoirse Ronan, Laurie Metcalf, Tracy Letts, Lucas Hedges, Beanie Feldstein, Timothee Chalamet",
+    genre: "Drama, Comedy",
+    runtime: 93,
+    link: "https://www.amazon.com/Lady-Bird-Saoirse-Ronan/dp/B07734STRN",
+    imgsrc:
+      "https://cdn.pastemagazine.com/www/articles/lady-bird-movie-poster.jpg",
+  },
+  {
+    title: "Moonlight",
+    director: "Barry Jenkins",
+    stars:
+      "Alex Hibbert, Ashton Sanders, Trevante Rhodes, Mahershala Ali, Naomie Harris, Janelle Monáe",
+    genre: "Drama",
+    runtime: 110,
+    link: "https://www.imdb.com/title/tt4975722/",
+    imgsrc:
+      "https://cdn.pastemagazine.com/www/articles/moonlight-movie-poster.jpg",
+  },
+  {
+    title: "I’m Thinking of Ending Things",
+    director: "Charlie Kaufman",
+    stars: "Jessie Buckley, Jesse Plemons, Toni Collette, David Thewlis",
+    genre: "Drama",
+    runtime: 134,
+    link: "https://www.netflix.com/in/title/80211559",
+    imgsrc:
+      "https://cdn.pastemagazine.com/www/articles/2020/09/11/im-thinking-of-ending.jpg",
+  },
+  {
+    title: "Lady Bird",
+    director: "Greta Gerwig",
+    stars:
+      "Saoirse Ronan, Laurie Metcalf, Tracy Letts, Lucas Hedges, Beanie Feldstein, Timothee Chalamet",
+    genre: "Drama, Comedy",
+    runtime: 93,
+    link: "https://www.amazon.com/Lady-Bird-Saoirse-Ronan/dp/B07734STRN",
+    imgsrc:
+      "https://cdn.pastemagazine.com/www/articles/lady-bird-movie-poster.jpg",
+  },
+  {
+    title: "Moonlight",
+    director: "Barry Jenkins",
+    stars:
+      "Alex Hibbert, Ashton Sanders, Trevante Rhodes, Mahershala Ali, Naomie Harris, Janelle Monáe",
+    genre: "Drama",
+    runtime: 110,
+    link: "https://www.imdb.com/title/tt4975722/",
+    imgsrc:
+      "https://cdn.pastemagazine.com/www/articles/moonlight-movie-poster.jpg",
+  },
+  {
+    title: "I’m Thinking of Ending Things",
+    director: "Charlie Kaufman",
+    stars: "Jessie Buckley, Jesse Plemons, Toni Collette, David Thewlis",
+    genre: "Drama",
+    runtime: 134,
+    link: "https://www.netflix.com/in/title/80211559",
+    imgsrc:
+      "https://cdn.pastemagazine.com/www/articles/2020/09/11/im-thinking-of-ending.jpg",
+  },
+  {
+    title: "Lady Bird",
+    director: "Greta Gerwig",
+    stars:
+      "Saoirse Ronan, Laurie Metcalf, Tracy Letts, Lucas Hedges, Beanie Feldstein, Timothee Chalamet",
+    genre: "Drama, Comedy",
+    runtime: 93,
+    link: "https://www.amazon.com/Lady-Bird-Saoirse-Ronan/dp/B07734STRN",
+    imgsrc:
+      "https://cdn.pastemagazine.com/www/articles/lady-bird-movie-poster.jpg",
+  },
+  {
+    title: "Moonlight",
+    director: "Barry Jenkins",
+    stars:
+      "Alex Hibbert, Ashton Sanders, Trevante Rhodes, Mahershala Ali, Naomie Harris, Janelle Monáe",
+    genre: "Drama",
+    runtime: 110,
+    link: "https://www.imdb.com/title/tt4975722/",
+    imgsrc:
+      "https://cdn.pastemagazine.com/www/articles/moonlight-movie-poster.jpg",
+  },
+  {
+    title: "I’m Thinking of Ending Things",
+    director: "Charlie Kaufman",
+    stars: "Jessie Buckley, Jesse Plemons, Toni Collette, David Thewlis",
+    genre: "Drama",
+    runtime: 134,
+    link: "https://www.netflix.com/in/title/80211559",
+    imgsrc:
+      "https://cdn.pastemagazine.com/www/articles/2020/09/11/im-thinking-of-ending.jpg",
+  },
+  {
+    title: "Lady Bird",
+    director: "Greta Gerwig",
+    stars:
+      "Saoirse Ronan, Laurie Metcalf, Tracy Letts, Lucas Hedges, Beanie Feldstein, Timothee Chalamet",
+    genre: "Drama, Comedy",
+    runtime: 93,
+    link: "https://www.amazon.com/Lady-Bird-Saoirse-Ronan/dp/B07734STRN",
+    imgsrc:
+      "https://cdn.pastemagazine.com/www/articles/lady-bird-movie-poster.jpg",
+  },
+  {
+    title: "Moonlight",
+    director: "Barry Jenkins",
+    stars:
+      "Alex Hibbert, Ashton Sanders, Trevante Rhodes, Mahershala Ali, Naomie Harris, Janelle Monáe",
+    genre: "Drama",
+    runtime: 110,
+    link: "https://www.imdb.com/title/tt4975722/",
+    imgsrc:
+      "https://cdn.pastemagazine.com/www/articles/moonlight-movie-poster.jpg",
+  },
+  {
+    title: "I’m Thinking of Ending Things",
+    director: "Charlie Kaufman",
+    stars: "Jessie Buckley, Jesse Plemons, Toni Collette, David Thewlis",
+    genre: "Drama",
+    runtime: 134,
+    link: "https://www.netflix.com/in/title/80211559",
+    imgsrc:
+      "https://cdn.pastemagazine.com/www/articles/2020/09/11/im-thinking-of-ending.jpg",
+  },
 ];
 
 export default ListData;
