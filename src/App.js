66222d2fc8b4af7fd96c7b15910c15f7609b58b9
Sonu Kumar <sonu.patna0808@gmail.com>
2021-05-11 09:55:28 +0530

    Server Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Checkout from "./components/MultilevelInputForm/FeedbackSystem";
+import FeedbackSystem from "./components/MultilevelInputForm/FeedbackSystem";
 
 function App() {
   return (
     <>
-      <Checkout />
+      <FeedbackSystem />
     </>
   );
 }
