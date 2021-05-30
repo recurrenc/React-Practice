79f08740873950053d9d9035e79d9355e04e0fda
Sonu Kumar <sonu.patna0808@gmail.com>
2021-05-30 22:46:56 +0530

    Server setup for Pdf Generator

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const bodyParser = require("body-parser");
+const pdf = require("html-pdf");
+const cors = require("cors");
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
