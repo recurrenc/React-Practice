e70ab1f56373ed83f3d8ab1acdb9553c200ea4a4
Sonu Kumar <sonu.patna0808@gmail.com>
2021-05-30 22:58:04 +0530

    create-pdf and fetch-pdf route added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const pdf = require("html-pdf");
 const cors = require("cors");
+const pdfTemplate = require("./documents");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,5 +10,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// /create-pdf route
+
+app.post("/create-pdf", (req, res) => {
+  pdf.create(pdfTemplate(req.body), {}).toFile("rezultati.pdf", (err) => {
+    if (err) {
+      return console.log("error");
+    }
+    res.send(Promise.resolve());
+  });
+});
+
+// /fetch-pdf route:
+app.get("/fetch-pdf", (req, res) => {
+  res.sendFile(`${__dirname}/rezultati.pdf`);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
