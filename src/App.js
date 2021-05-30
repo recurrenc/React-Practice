acced3f9700692055f2fb71fcaa8cce3385ffed4
Sonu Kumar <sonu.patna0808@gmail.com>
2021-05-30 23:39:55 +0530

    Connect client and server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import CovidIndia from "./components/covidTracker/CovidIndia";
+import HandlePdf from "./components/PDFGenerator/HandlePdf";
+// import CovidIndia from "./components/covidTracker/CovidIndia";
 // import Admin from "./components/MultilevelInputForm/Admin/AdminMain/Admin";
 // import FeedbackSystem from "./components/MultilevelInputForm/FeedbackSystem";
 
@@ -9,7 +10,8 @@ function App() {
     <>
       {/* <FeedbackSystem /> */}
       {/* <Admin /> */}
-      <CovidIndia />
+      {/* <CovidIndia /> */}
+      <HandlePdf />
     </>
   );
 }
---
+++ src/components/PDFGenerator/HandlePdf.js
@@ -0,0 +1,51 @@
+import React from "react";
+import axios from "axios";
+import { saveAs } from "file-saver";
+
+const HandlePdf = () => {
+  const [state, setState] = React.useState({
+    name: "Adrian",
+    receiptId: 0,
+    price1: 0,
+    price2: 0,
+  });
+
+  const handleChange = ({ target: { value, name } }) =>
+    setState({ [name]: value });
+
+  const createAndDownloadPdf = () => {
+    axios.post("/create-pdf", state);
+  };
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Receipt ID"
+        name="receiptId"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Price 1"
+        name="price1"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Price 2"
+        name="price2"
+        onChange={handleChange}
+      />
+      <button onClick={createAndDownloadPdf}>Download PDF</button>
+    </div>
+  );
+};
+
+export default HandlePdf;
